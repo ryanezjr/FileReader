5141a9d7373db09e68f22213b6405d795e8f8c05
Raul Yanez <ryanez@raul-macbook-pro2.attlocal.net>
2019-02-04 09:00:45 -0600

    Made read function static

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@
 import PackageDescription
 
 let package = Package(
+
     name: "FileReader",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
